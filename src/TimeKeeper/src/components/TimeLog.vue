ca9f813ce11c3d320e9e0f0056716af601ce6ea6
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2021-10-03 23:42:17 +0100

    WIP Light Dark Theme

---
+++ src/TimeKeeper/src/components/TimeLog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="time-log">
+    <h1>{{ msg }}</h1>
+    <h3>Installed CLI Plugins</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class TimeLog extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
